14c72b55d1905e1ecd314d7f2b060e271bc7a032
lvhoushuai <lvhoushuai_vendor@sensetime.com>
2022-06-17 17:57:06 +0800

    add log

---
+++ app/log.go
@@ -0,0 +1,56 @@
+package app
+
+import (
+	"encoding/json"
+	"fmt"
+
+	code "github.com/tsxylhs/go-starter"
+
+	"github.com/spf13/viper"
+	"github.com/tsxylhs/go-starter/app/log"
+	"go.uber.org/zap"
+)
+
+const (
+	StarterLog        = "LOG"
+	ConfigFileNameLog = "log_config"
+)
+
+type LogStarter struct {
+	*BaseStarter
+}
+
+func (starter *LogStarter) Start(ctx *code.Context) error {
+	var logConfig zap.Config
+	var conf *viper.Viper = viper.New()
+	var err error
+
+	err = LoadConfig(ConfigFileNameLog, conf)
+	if err != nil {
+		return err
+	}
+	m := map[string]interface{}{}
+	if err = conf.UnmarshalKey("log", &m); err != nil {
+		return err
+	}
+
+	logcfgs, _ := json.Marshal(m)
+	fmt.Println("log config: \n" + string(logcfgs))
+
+	if err := json.Unmarshal(logcfgs, &logConfig); err != nil {
+		return err
+	}
+
+	if log.Logger.Logger, err = logConfig.Build(); err != nil {
+		return err
+	}
+	log.Slog = log.Logger.Sugar()
+
+	log.Logger.Info("logger inited")
+
+	return nil
+}
+
+func init() {
+	RegisterStarter(&LogStarter{BaseStarter: NewBaseStarter(StarterLog, PriorityHigh+100)})
+}
